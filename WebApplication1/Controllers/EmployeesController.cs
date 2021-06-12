d19040ffe91413878302c69de9442f23ee7f3f98
Shashwat Sharma <shashwats15@gmail.com>
2021-06-12 13:54:16 +0530

    first Entity App

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary1;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get() 
+        {
+            using (EmployeeDbEntities Entities = new EmployeeDbEntities())
+            {
+               return Entities.Employees.ToList();
+            } 
+        }
+
+        public Employee Get( int Id)
+        {
+            using (EmployeeDbEntities Entities = new EmployeeDbEntities())
+            {
+                return Entities.Employees.FirstOrDefault(e => e.ID == Id);
+            }
+        }
+    }
+}
